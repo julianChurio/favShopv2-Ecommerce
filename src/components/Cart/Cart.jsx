62996239420e0dacd94eb8c76824f4bdc25b95f9
julianChurio <julichurio282@gmail.com>
2022-10-03 10:17:59 -0300

    desafio cart collection 2, cargando una orden de compra a firestore

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,28 +1,79 @@
 import { useCartContext } from "../../CartProvider";
 import { Link } from "react-router-dom";
 import CartItem from "./CartItem";
+import { addDoc, getFirestore } from 'firebase/firestore';
+import { collection } from 'firebase/firestore';
 
 const Cart = () => {
   const { cart, precioTotal } = useCartContext();
 
+  const ordenDeCompra = {
+    comprador: {
+      nombre: "Juli",
+      email: "julichurio282@gmail.com",
+      telefono: "2234976683",
+      direccion: "asdasdas",
+    },
+    items: cart.map((product) => ({
+      id: product.id,
+      title: product.nombre,
+      cantidad: product.cantidad,
+      precioPorUnidad: product.precio,
+      subTotal: product.cantidad * product.precio,
+    })),
+    total: { precioTotal },
+  };
+
+  const mandarCompra = () => {
+    const traerFirestore = getFirestore()
+    const collecionDeOrdenes = collection(traerFirestore, 'ordenes')
+    addDoc(collecionDeOrdenes, ordenDeCompra)
+    .then(({id}) => console.log(id))
+  }
+
+
+
   if (cart.length === 0) {
     return (
-      <>
+      <div className="cart">
         <h1>No hay elementos en el carrito</h1>
         <Link to="/catalogo/">
           <button>Hacer compras</button>
         </Link>
-      </>
+      </div>
     );
   }
 
   return (
-    <>
-      {cart.map((product) => (
-        <CartItem key={product.id} product={product} />
-      ))}
+    <div className="cart">
+      <table>
+        <tbody className="tableRow">
+          <th>
+            <p className="tableHeader">Eliminar</p>
+          </th>
+          <th>
+            <p className="tableHeader">Imagen</p>
+          </th>
+          <th>
+            <p className="tableHeader">Nombre</p>
+          </th>
+          <th>
+            <p className="tableHeader">Cantidad</p>
+          </th>
+          <th>
+            <p className="tableHeader">Precio por unidad</p>
+          </th>
+          <th>
+            <p className="tableHeader">Subtotal</p>
+          </th>
+        </tbody>
+        {cart.map((product) => (
+          <CartItem key={product.id} product={product} />
+        ))}
+      </table>
       <p>Precio total: AR${precioTotal}</p>
-    </>
+      <button onClick={mandarCompra()}>Finalizar Compra</button>
+    </div>
   );
 };
 
--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,24 +1,44 @@
 import React from "react";
 import { useCartContext } from "./../../CartProvider";
+import "./index.css";
 
 const CartItem = ({ product }) => {
   const { removerProducto } = useCartContext();
-  if(product.precio === "Gratis") {
-    product.precio = 0
-  }
 
   return (
-    <div>
-      <img src={product.photo} alt="" />
-      <div>
-        <h1>Titulo : {product.nombre}</h1>
-        <p>Cantidad: {product.cantidad}</p>
-        <p>Precio : {product.precio}</p>
-        <p>Subtotal : ${product.cantidad * product.precio}</p>
+    <tr className="tableRowItems">
+      <td>
         <button onClick={() => removerProducto(product.id)}>Eliminar</button>
-      </div>
-    </div>
+      </td>
+      <td className="tablePhoto">
+        <img src={product.photo} alt="" />
+      </td>
+      <td>
+        <p>{product.nombre}</p>
+      </td>
+      <td>
+        <p>{product.cantidad}</p>
+      </td>
+      <td>
+        <p>{product.precio}</p>
+      </td>
+      <td>
+        <p>${product.cantidad * product.precio}</p>
+      </td>
+    </tr>
   );
 };
 
 export default CartItem;
+
+/* 
+
+      <img src={product.photo} alt="" />
+      <h1>{product.nombre}</h1>
+      <p>{product.cantidad}</p>
+      <p>{product.precio}</p>
+      <p>${product.cantidad * product.precio}</p>
+      <button onClick={() => removerProducto(product.id)}>Eliminar</button>
+
+
+*/
