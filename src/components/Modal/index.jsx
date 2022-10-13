1ae11689166e3e1dacdbfb0156dab16950dafd2a
julianChurio <julichurio282@gmail.com>
2022-10-13 16:31:47 -0300

    modal

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 function Popup({ funcion, getValuesFromModal }) {
   const [show, setShow] = useState(false);
-  const [data, setData] = useState({});
 
   const [name, setName] = useState();
   const [email, setEmail] = useState();
@@ -12,9 +11,10 @@ function Popup({ funcion, getValuesFromModal }) {
 
   const handleClose = () => {
     getValuesFromModal(name, email, number);
-    funcion()
+    funcion();
     setShow(false);
   };
+
   const handleShow = () => setShow(true);
 
   return (
@@ -26,11 +26,11 @@ function Popup({ funcion, getValuesFromModal }) {
           <Modal.Title>Ingresar datos</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="modal-body">
-            <input type="text" placeholder="Nombre" id="nombre" onChange={(e)=> setName(e.target.value)} />
-            <input type="email" placeholder="Email" id="email" onChange={(e)=> setEmail(e.target.value)} />
-            <input type="number" placeholder="Numero de telefono" id="numero" onChange={(e)=> setNumber(e.target.value)} />
-          </div>
+          <form className="modal-body">
+            <input type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+            <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input type="number" placeholder="Number" onChange={(e) => setNumber(e.target.value)} />
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
